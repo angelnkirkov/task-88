a0bb72f1f487871c4644ef01fd79de8dc5578414
Angel Kirkov <angelnkirkov@gmail.com>
2021-09-18 15:03:50 +0300

    edit

--- src/js/Application.js
+++ src/js/Application.js
@@ -12,5 +12,15 @@ export default class Application extends EventEmitter {
     this.init();
     this.emit(Application.events.READY);
   }
-  init() {}
+  init() {
+    let article1 = document.querySelector(".article");
+    anime({
+      targets: ".article",
+      translateX: 250,
+      direction: "alternate",
+      loop: true,
+      easing: "spring(1, 80, 10, 0)",
+    });
+    article1.addEventListener("click", anime);
+  }
 }
